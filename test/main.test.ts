59db504b7df088b688aab5c3f1d303bbbf4f2463
aaronbrighton <aaronbrighton@users.noreply.github.com>
2021-09-11 00:14:48 +0000

    chore(deps): upgrade dependencies
    Upgrades project dependencies. See details in [workflow run].
    
    [Workflow Run]: https://github.com/aaronbrighton/aaronbrighton.ca/actions/runs/1223154545
    
    ------
    
    *Automatically created by projen via the "upgrade-dependencies" workflow*

--- test/main.test.ts
+++ test/main.test.ts
@@ -3,6 +3,6 @@ import '@aws-cdk/assert/jest';
 test('Snapshot', () => {
   // Bypassing tests for now due to following unresolved error:
   //
-  // Stack 'prod-aaronbrighton-ca-stack' does not have deployment role information; use the 'DefaultStackSynthesizer' synthesizer, or set the '@aws-cdk/core:newStyleStackSynthesis' context key. 
+  // Stack 'prod-aaronbrighton-ca-stack' does not have deployment role information; use the 'DefaultStackSynthesizer' synthesizer, or set the '@aws-cdk/core:newStyleStackSynthesis' context key.
   //
 });
