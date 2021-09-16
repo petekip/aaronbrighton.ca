d91d0af884d244508746c162aa81722f11e23e67
Aaron Brighton <aaron@aaronbrighton.ca>
2021-09-16 14:46:35 -0400

    Correcting a copy paste error

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import { AaronBrightonCaStack } from './aaronbrighton-ca';
 
 const app = new App();
 
-new AaronBrightonCaStack(app, 'deployed-scream-elon-com'); // Try to line up this identifier with the one in pipeline.ts
+new AaronBrightonCaStack(app, 'deployed-aaronbrighton-ca'); // Try to line up this identifier with the one in pipeline.ts
 
 app.synth();
